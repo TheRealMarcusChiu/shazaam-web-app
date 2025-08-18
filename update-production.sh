2b1ef01b58e2c5bf4902a809fadc2b41fd1d585a
Marcus Chiu <marcuschiu9@gmail.com>
2025-08-18 08:34:41 -0500

    initial commit

--- update-production.sh
+++ update-production.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
 ssh aws << EOF
-  rm -rf shazam-webapp/
-  mkdir shazam-webapp
+  cd shazam-web-app/
+  git pull
 EOF
-
-scp -i ~/.ssh/keys/aws-marcuschiu.pem -r ./html ec2-user@www.marcuschiu.com:~/shazam-webapp
